b8add5ea42ed75e07f604fdfbd090cc4ddf5400f
wuqi11 <wuqi11@baidu.com>
2019-08-03 18:29:43 +0800

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+        String name1 = "wuqi";
+        String name2 = "zhangsan";
+        String name4 = "lisi";
+        String name5 = "wangermazi";
+
+    }
+
+
+}
